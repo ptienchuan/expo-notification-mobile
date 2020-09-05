818f0032b37d8448f30bfdd642b9154544e10257
ptienchuan <ptienchuan@gmail.com>
2020-09-05 11:42:37 +0700

    Add safeAreaView

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, EffectCallback } from 'react';
-import { AppLoading } from 'expo';
-import * as Font from 'expo-font';
-import MainNavigator from './src/navigators/MainNavigator';
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, StyleSheet, Platform } from "react-native";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import MainNavigator from "./src/navigators/MainNavigator";
 
 function App() {
   const [isReady, setIsReady] = useState(false);
@@ -9,19 +10,47 @@ function App() {
   useEffect(() => {
     if (!isReady) {
       Font.loadAsync({
-        Roboto: require('native-base/Fonts/Roboto.ttf'),
-        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
       })
         .then(() => {
           setIsReady(true);
         })
         .catch((err) => {
-          console.log('Loading font occurs error: ', err);
+          console.log("Loading font occurs error: ", err);
         });
     }
   });
 
-  return isReady ? <MainNavigator /> : <AppLoading />;
+  return isReady ? (
+    <SafeAreaView
+      style={[
+        styles.container,
+        Platform.select({
+          ios: styles.containerIOS,
+          android: styles.containerAndroid,
+        }),
+      ]}
+    >
+      <MainNavigator />
+    </SafeAreaView>
+  ) : (
+    <AppLoading />
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  containerIOS: {
+    backgroundColor: "gray",
+    paddingTop: 0,
+  },
+  containerAndroid: {
+    backgroundColor: "white",
+    paddingTop: 25,
+  },
+});
 
 export default App;
