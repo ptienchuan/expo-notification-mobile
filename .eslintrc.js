ab193ee79b5f9759c6385c8072c244f6bda4c5ce
ptienchuan <ptienchuan@gmail.com>
2020-10-31 10:54:07 +0700

    Create notification service

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,32 +2,33 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
-    "react-native/react-native": true,
+    'react-native/react-native': true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "react-native", "@typescript-eslint"],
+  plugins: ['react', 'react-native', '@typescript-eslint'],
   rules: {
-    "react-native/no-unused-styles": 2,
-    "react-native/no-inline-styles": 2,
-    "react-native/no-raw-text": 2,
-    "react-native/no-single-element-style-arrays": 2,
-    "react/display-name": 0,
+    'react-native/no-unused-styles': 'error',
+    'react-native/no-inline-styles': 'error',
+    'react-native/no-raw-text': 'error',
+    'react-native/no-single-element-style-arrays': 'error',
+    'react/display-name': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
   settings: {
     react: {
-      version: "detect"
-    }
-  }
+      version: 'detect',
+    },
+  },
 };
